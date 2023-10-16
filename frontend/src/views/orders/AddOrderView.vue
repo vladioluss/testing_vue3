33d18c2fc0b5e9ac3ef76a2c34101542f7deff42
Vladislav <grishin1999@list.ru>
2023-10-16 03:27:04 +0300

    обязательные поля

--- frontend/src/views/orders/AddOrderView.vue
+++ frontend/src/views/orders/AddOrderView.vue
@@ -2,17 +2,19 @@
 import BaseForm from "@/components/ui/forms/BaseForm.vue";
 import BaseButton from "@/components/ui/buttons/BaseButton.vue";
 import BaseInput from "@/components/ui/inputs/BaseInput.vue";
-import {ref} from "vue";
-import {useEventsStore} from "@/stores/modules/events";
+import {computed, ref} from "vue";
 import router from "@/router";
 import {usePreloaderStore} from "@/stores/modules/base";
 import {postItem} from "@/app/api";
 import type {EventInterface} from "@/types/order";
+import {useAuthStore} from "@/stores/modules/auth";
+
+const auth = useAuthStore()
 
 const URL: string = 'events'
 
 // Данные для формы
-const name = ref<string>('')
+const name = ref<string>(auth.user.name)
 const address = ref<string>('')
 const comment = ref<string>('')
 const status: string = 'Новый'
@@ -26,65 +28,72 @@ date = date.replace(' г.', '')
 // const eventsStore = useEventsStore()
 const preloader = usePreloaderStore()
 
+const valid = computed(() => name.value !== '' && address.value !== '')
+
 function createEvent(): void {
-  // ВКЛ прелоадер
-  preloader.setActivePreloader(true)
+  if (valid.value) {
+    // ВКЛ прелоадер
+    preloader.setActivePreloader(true)
 
-  const params:EventInterface = {
-    'name': name.value,
-    'address': address.value,
-    'date': date,
-    'status': status,
-    'comment': comment.value,
+    const params: EventInterface = {
+      'name': name.value,
+      'address': address.value,
+      'date': date,
+      'status': status,
+      'comment': comment.value,
+    }
+
+    // Добавляем запись в БД
+    postItem(URL, params)
+        .then(() => router.push({name: 'orders'}))
+        .finally(() => preloader.setActivePreloader(false))
   }
-
-  // Добавляем запись в БД
-  postItem(URL, params)
-      .then(() => router.push({ name: 'orders'}))
-      .finally(() => preloader.setActivePreloader(false))
 }
 </script>
 
 <template>
-<div class="add-order">
-  <p>Добавить заказ</p>
-  <BaseForm>
-    <template #inputs>
-      <BaseInput
-        :type="'text'"
-        :placeholder="'Введите ваше имя'"
-        :label="'Ваше имя'"
-        v-model="name"
-      />
-      <BaseInput
-          :type="'text'"
-          :placeholder="'Введите ваш адрес'"
-          :label="'Ваш адрес'"
-          v-model="address"
-      />
-      <BaseInput
-          :type="'text'"
-          :placeholder="'Комментарий'"
-          :label="'Комментарий'"
-          v-model="comment"
-      />
-    </template>
-    <template #buttons>
-      <BaseButton @click="createEvent">
-        <template #text>Добавить заказ</template>
-      </BaseButton>
-    </template>
-  </BaseForm>
-</div>
+  <div class="add-order">
+    <p>Добавить заказ</p>
+    <BaseForm>
+      <template #inputs>
+        <BaseInput
+            required
+            :type="'text'"
+            :placeholder="'Введите ваше имя'"
+            :label="'Ваше имя*'"
+            v-model="name"
+        />
+        <BaseInput
+            required
+            :type="'text'"
+            :placeholder="'Введите ваш адрес'"
+            :label="'Ваш адрес*'"
+            v-model="address"
+        />
+        <BaseInput
+            :type="'text'"
+            :placeholder="'Комментарий'"
+            :label="'Комментарий'"
+            v-model="comment"
+        />
+      </template>
+      <template #buttons>
+        <BaseButton @click="createEvent">
+          <template #text>Добавить заказ</template>
+        </BaseButton>
+      </template>
+    </BaseForm>
+    <div v-if="!valid">Заполните обязательные поля</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-  .add-order {
-    padding: 30px;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 30px;
-  }
+.add-order {
+  padding: 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+}
 </style>
