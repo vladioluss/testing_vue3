d491de3c6006f53ce3b30208304b306f0adb8bd1
Vladislav <grishin1999@list.ru>
2023-10-17 18:23:30 +0300

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/',
+  base: '/testing_vue3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
